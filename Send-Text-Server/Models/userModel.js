b791ef2bad02e86e70111d7a3aab224ff0331a87
Miran Ibrahim <moeenuddin.miran@gmail.com>
2024-01-03 21:20:31 +0600

    server created

---
+++ Send-Text-Server/Models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    pic: {
+      type: String,
+      required: true,
+      default: "https://cdn-icons-png.flaticon.com/512/9131/9131529.png",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
